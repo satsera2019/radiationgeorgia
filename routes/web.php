6ae3d06c668a4c9a389d60cf5071022523c62c36
satsera2019 <saba.satseradze0@gmail.com>
2023-02-26 18:42:05 +0400

    init main: controoler , view, route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [MainController::class, 'index'])->name('index');
