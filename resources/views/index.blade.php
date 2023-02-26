9f50dc2b127309b4a9359108e409c27604943e7c
satsera2019 <saba.satseradze0@gmail.com>
2023-02-26 19:49:55 +0400

    add iframe

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,10 @@
-<div>
-    მთავარი გვერდი
-</div>
+<!DOCTYPE html>
+<html>
+<body>
+
+<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 
+src="https://maps.google.com/maps?q=Tbilisi&amp;ie=UTF8&amp;&amp;output=embed"></iframe><br />
+
+
+</body>
+</html>
