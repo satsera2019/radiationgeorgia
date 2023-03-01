af9d39b725c21bfc09f8dc937d7524090ecefc43
satsera2019 <saba.satseradze0@gmail.com>
2023-03-01 15:09:41 +0400

    change ol v3 to v7
    change version

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,147 +1,268 @@
-<!doctype html>
+<!DOCTYPE html>
 <html>
 
 <head>
-    <title>Hello OpenStreetMap</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <link rel="stylesheet" href="../assets/ol3/css/ol.css" type="text/css" />
-    <link rel="stylesheet" href="../assets/css/samples.css" type="text/css" />
+
+    <title>Popup</title>
+
+    {{-- ol7/css/ol.css --}}
+    <link rel="stylesheet" href="{{ asset('assets/ol7/css/ol.css') }}" />
+    {{-- ol7/js/ol.js --}}
+    <script src="{{ asset('assets/ol7/js/ol.js') }}"></script>
+
+    <style>
+        html,
+        body,
+        .map {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+        }
+
+        .ol-popup {
+            position: absolute;
+            background-color: white;
+            -webkit-filter: drop-shadow(0 1px 4px rgba(0, 0, 0, 0.2));
+            filter: drop-shadow(0 1px 4px rgba(0, 0, 0, 0.2));
+            padding: 15px;
+            border-radius: 10px;
+            border: 1px solid #cccccc;
+            bottom: 12px;
+            left: -50px;
+            min-width: 280px;
+        }
+
+        .ol-popup:after,
+        .ol-popup:before {
+            top: 100%;
+            border: solid transparent;
+            content: " ";
+            height: 0;
+            width: 0;
+            position: absolute;
+            pointer-events: none;
+        }
+
+        .ol-popup:after {
+            border-top-color: white;
+            border-width: 10px;
+            left: 48px;
+            margin-left: -10px;
+        }
+
+        .ol-popup:before {
+            border-top-color: #cccccc;
+            border-width: 11px;
+            left: 48px;
+            margin-left: -11px;
+        }
+
+        .ol-popup-closer {
+            text-decoration: none;
+            position: absolute;
+            top: 2px;
+            right: 8px;
+        }
+
+        .ol-popup-closer:after {
+            content: "✖";
+        }
+
+        .ol-marker .ol-icon {
+            cursor: pointer !important;
+        }
+    </style>
 
 </head>
 
 <body>
-    <div id="map" class="full-map"></div>
-    <script src="../assets/ol3/js/ol.js"></script>
-    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <div id="map" class="map"></div>
 
-    {{-- <script>
+    <div id="popup" class="ol-popup">
+        <a href="#" id="popup-closer" class="ol-popup-closer"></a>
+        <div id="popup-content"></div>
+    </div>
+
+
+
+    {{-- <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script> --}}
+    {{-- ol.js --}}
+    {{-- <script src="../assets/ol3/js/ol.js"></script> --}}
+
+
+
+    <script>
+        /**
+         * Elements that make up the popup.
+         */
+        const container = document.getElementById('popup');
+        const content = document.getElementById('popup-content');
+        const closer = document.getElementById('popup-closer');
+
+        /**
+         * Create an overlay to anchor the popup to the map.
+         */
+        const overlay = new ol.Overlay({
+            element: container,
+            autoPan: {
+                animation: {
+                    duration: 250,
+                },
+            },
+        });
+
+        /**
+         * Add a click handler to hide the popup.
+         * @return {boolean} Don't follow the href.
+         */
+        closer.onclick = function() {
+            overlay.setPosition(undefined);
+            closer.blur();
+            return false;
+        };
+
+        const Model = [{
+                category: 'Capital city',
+                title: 'London',
+                long: 44.76488828241705,
+                lat: 41.72839858048209,
+                description: 'UK capital',
+            },
+            {
+                category: 'Capital city',
+                title: 'Rome',
+                long: 44.7894181004358,
+                lat: 41.706101338417945,
+                description: 'Italy capital',
+            },
+        ];
+
+
         // center location
         let lat = 41.7415928;
         let lng = 44.7977544;
-
-        // the source of our photos is a KML file
-        var flickrSource = new ol.source.KML({
-            url: '../assets/data/flickr_data.kml',
-            projection: 'EPSG:3857',
-            extractStyles: false
-        });
-
-        // use a style function to replace the styles in the KML file
-        // this is a placeholder for now
-        function flickrStyle(feature) {
-            var style = new ol.style.Style({
-                image: new ol.style.Circle({
-                    // url: "../assets/img/dog.png",
-                    radius: 16,
-                    stroke: new ol.style.Stroke({
-                        color: 'white',
-                        width: 2
-                    }),
-                    fill: new ol.style.Fill({
-                        color: 'green'
-                    })
-                })
-            });
-            return [style];
-        }
-
-        var flickrLayer = new ol.layer.Vector({
-            source: flickrSource,
-            style: flickrStyle
-        });
-
-        var layer = new ol.layer.Tile({
-            source: new ol.source.OSM()
-        });
-
         // Create latitude and longitude and convert them to default projection
-        var tbilisi = ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857');
+        let tbilisi = ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857');
 
-        var view = new ol.View({
-            center: tbilisi,
-            zoom: 2
-        });
-
-        var map = new ol.Map({
-            target: 'map',
-            layers: [layer, flickrLayer],
-            view: view
-        });
-    </script> --}}
-
-    <script>
-        let lat = 41.7415928;
-        let lng = 44.7977544;
-
-        var tbilisi = ol.proj.transform([lng, lat], 'EPSG:4326', 'EPSG:3857');
-        var view = new ol.View({
-            center: tbilisi,
-            zoom: 12
-        });
-
-        var vectorSource = new ol.source.Vector({});
-        var places = [
-            [44.76488828241705, 41.72839858048209, 'http://maps.google.com/mapfiles/ms/micons/blue.png', 'red'],
-            [44.7894181004358, 41.706101338417945, 'http://maps.google.com/mapfiles/ms/micons/blue.png', 'green'],
-        ];
-
-        var features = [];
-        for (var i = 0; i < places.length; i++) {
-            var iconFeature = new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.transform([places[i][0], places[i][1]], 'EPSG:4326',
-                    'EPSG:3857')),
-            });
-            var iconStyle = new ol.style.Style({
-                image: new ol.style.Icon({
-                    src: places[i][2],
-                    crossOrigin: 'anonymous',
-                    color: places[i][3],
-                })
-            });
-            iconFeature.setStyle(iconStyle);
-            vectorSource.addFeature(iconFeature);
-        }
-
-        var vectorLayer = new ol.layer.Vector({
-            source: vectorSource,
-            updateWhileAnimating: true,
-            updateWhileInteracting: true
-        });
-
-        var map = new ol.Map({
-            target: 'map',
-            view: view,
+        /**
+         * Create the map.
+         */
+        const map = new ol.Map({
             layers: [
                 new ol.layer.Tile({
-                    preload: 3,
                     source: new ol.source.OSM(),
                 }),
-                vectorLayer,
             ],
-            loadTilesWhileAnimating: true,
+            overlays: [overlay],
+            target: 'map',
+            view: new ol.View({
+                center: ol.proj.fromLonLat([lng, lat]),
+                zoom: 11,
+                minZoom: 11,
+            }),
         });
 
-        // bounce after reload
-        doBounce(tbilisi);
-
-        function doBounce(location) {
-            // bounce by zooming out one level and back in
-            var bounce = ol.animation.bounce({
-                resolution: map.getView().getResolution() * 2
+        const features = [];
+        for (key in Model) {
+            const _Data = Model[key];
+            const feature = new ol.Feature({
+                geometry: new ol.geom.Point(
+                    ol.proj.fromLonLat([parseFloat(_Data.long), parseFloat(_Data.lat)])
+                ),
+                category: _Data.category,
+                title: _Data.title,
+                description: _Data.description,
             });
-            // start the pan at the current center of the map
-            var pan = ol.animation.pan({
-                source: map.getView().getCenter()
-            });
-            map.beforeRender(bounce);
-            map.beforeRender(pan);
-            // when we set the center to the new location, the animated move will
-            // trigger the bounce and pan effects
-            map.getView().setCenter(location);
+            features.push(feature);
         }
+
+        const markers = new ol.layer.Vector({
+            source: new ol.source.Vector({
+                features: features,
+            }),
+            style: new ol.style.Style({
+                image: new ol.style.Icon({
+                    anchor: [0.5, 1],
+                    src: 'https://ucarecdn.com/4b516de9-d43d-4b75-9f0f-ab0916bd85eb/marker.png',
+                }),
+                cursor: 'pointer',
+            }),
+        });
+
+        map.addLayer(markers);
+
+        map.on('click', function(evt) {
+            const feature = map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+                return feature;
+            });
+            if (feature) {
+                const coordinates = feature.getGeometry().getCoordinates();
+                content.innerHTML =
+                    '<p>Category:</p><code>' + feature.get('category') + '</code><br>' +
+                    '<p>Title:</p><code>' + feature.get('title') + '</code><br>' +
+                    '<p>Description:</p><code>' + feature.get('description') + '</code>'
+                overlay.setPosition(coordinates);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+        // bounce after reload
+        // doBounce(tbilisi);
+
+        // function doBounce(location) {
+        //     // bounce by zooming out one level and back in
+        //     var bounce = ol.animation.bounce({
+        //         resolution: map.getView().getResolution() * 2
+        //     });
+        //     // start the pan at the current center of the map
+        //     var pan = ol.animation.pan({
+        //         source: map.getView().getCenter()
+        //     });
+        //     map.beforeRender(bounce);
+        //     map.beforeRender(pan);
+        //     // when we set the center to the new location, the animated move will
+        //     // trigger the bounce and pan effects
+        //     map.getView().setCenter(location);
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /* marker cursor pointer start */
+        map.on('pointermove', evt => {
+            if (!evt.dragging) {
+                map.getTargetElement().style.cursor = map.hasFeatureAtPixel(map.getEventPixel(evt
+                    .originalEvent)) ? 'pointer' : '';
+            }
+        });
+        /* marker cursor pointer end */
     </script>
-
-
 </body>
 
 </html>
